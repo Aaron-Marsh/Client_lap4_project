ea8dad558304b52a975f636b42745ad73a75290c
Chris Pucknell <chris.pucknell@gmail.comgit config --global -lgit config --global user.email chris.pucknell@gmail.com>
2022-07-14 16:49:24 +0100

    add pseudo to Search page

--- src/pages/Search/index.js
+++ src/pages/Search/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
-
+// Change to search results?
 function Search() {
     return(
         <>
             <h2>Search page</h2>
+            {/* search results here */}
         </>
     )
 }
