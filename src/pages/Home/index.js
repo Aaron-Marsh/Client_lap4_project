d20d7018a6f1f70f50af20d65bba1fc1280dd652
Chris Pucknell <chris.pucknell@gmail.comgit config --global -lgit config --global user.email chris.pucknell@gmail.com>
2022-07-14 17:03:58 +0100

    add pseudo code to pages, add searchbar comp to home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -4,7 +4,13 @@ import React from 'react';
 function Home() {
     return(
         <>
-            <h2>Home page</h2>
+            <h2>Home page</h2> 
+            {/* hero image */}
+            {/* Books of the Week link */}
+            {/* Register Button? */}
+            {/* User Login Button? */}
+            <SearchBar/>
+            {/* footer? */}
         </>
     )
 }
