c6abdf63e44f2ee6c2c67daf01e96ed64c869337
Rebecca Blixt <rebecca.blixt@hotmail.com>
2022-07-15 09:49:42 +0200

    export SearchPage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
-export { BooksPage } from "./BooksPage";
-export { ForumsPage } from "./ForumsPage";
-export { HomePage } from "./HomePage";
-export { ProfilePage } from "./ProfilePage";
+export { BooksPage } from './BooksPage';
+export { ForumsPage } from './ForumsPage';
+export { HomePage } from './HomePage';
+export { ProfilePage } from './ProfilePage';
+export { SearchPage } from './SearchPage';
