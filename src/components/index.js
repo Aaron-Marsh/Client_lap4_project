2bddfa55d300b31a71b7445132784a7f1a1ba543
Izgardon <williamsessions23@gmail.com>
2022-07-15 18:40:12 +0100

    adding javascript to footer to only appear after scroll

--- src/components/index.js
+++ src/components/index.js
@@ -6,3 +6,4 @@ export { Navbar } from "./Navbar";
 export { Profile } from "./Profile";
 export { SearchBar } from "./SearchBar";
 export { LoginModal } from "./LoginModal";
+export { LoginFooter } from "./LoginFooter";
