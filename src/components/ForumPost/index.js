ee9d6752320f65e29cee17ffaf6071a7b1b0501a
Chris Pucknell <chris.pucknell@gmail.comgit config --global -lgit config --global user.email chris.pucknell@gmail.com>
2022-07-16 23:44:36 +0100

    add fresh prince of bel air to ForumPost placeholder text

--- src/components/ForumPost/index.js
+++ src/components/ForumPost/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import './style.css';
 
 export const ForumPost = () => {
 	return (
-		<>
-			<h2>Forum threads</h2>
-			<p>Here are loads of interesting threads</p>
-		</>
+		<div className="container post">
+			<h2>Forum thread</h2>
+			<p>Now this is a story all about how my life got flipped turned upside down and I'd like to take a minute, just sit right there, I'll tell you how I became the prince of a town called Bel-Air</p>
+		</div>
 	);
 };
