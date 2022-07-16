9e9e4c1feabd97fef417e3e05022bd196b8efd09
Chris Pucknell <chris.pucknell@gmail.comgit config --global -lgit config --global user.email chris.pucknell@gmail.com>
2022-07-16 23:45:37 +0100

    update ForumsPage for new component ForumPosts

--- src/pages/ForumsPage/index.js
+++ src/pages/ForumsPage/index.js
@@ -1,42 +1,14 @@
-import React, { useState, useEffect } from 'react';
-import { ForumPost } from '../../components/index';
+import React from 'react';
+import { ForumPosts } from '../../components/index';
 import { SearchBar } from '../../components/index';
 
 export const ForumsPage = () => {
-
-	const [posts,setPosts] = useState([
-		'some post',
-		'some post',
-		'some post']);
-
-	// the bit what gets all the posts
-	// const fetchPosts = async () => {
-	// 	try {
-
-	// 		const { data } = await axios.get('');
-	// 		console.log("DATA:",data);
-	// 		setPosts(data);
-	// 	} catch (err) {
-	// 		throw new Error(err.message);
-	// 	}
-	// }
-
-	// useEffect(()=> {
-	// 	fetchPosts();
-	// },[])
-
-	// <ForumPost title=(post.title)/>
-	// {JSON.stringify(post)}
 
 	return (
 		<div class="container">
 			<h2>Forums page</h2>
 			<SearchBar />
-			<div class="post-wrapper">
-			{posts.map((post)=>(
-				<p>{post}</p>
-				))}
-			</div>
+			<ForumPosts/>
 		</div>
 	);
 };
