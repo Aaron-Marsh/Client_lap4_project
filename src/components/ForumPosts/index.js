6edaaaf58fa0466d5f97c90c8029a29e33a3f42f
Chris Pucknell <chris.pucknell@gmail.comgit config --global -lgit config --global user.email chris.pucknell@gmail.com>
2022-07-17 22:00:42 +0100

    update ForumPosts to render Posts

--- src/components/ForumPosts/index.js
+++ src/components/ForumPosts/index.js
@@ -1,46 +1,66 @@
-import React, { useState, useEffect } from 'react';
-import {ForumPost} from '../index'
-import { Link } from 'react-router-dom';
+import React, { useState,useEffect } from "react";
+import { ForumPost } from "../index";
+import { Link } from "react-router-dom";
+import axios from "axios";
+// import { useSelector } from "react-redux";
 
 export const ForumPosts = () => {
+    // const storeServerURL = useSelector((state) => state.serverReducer);
 
-	const [posts,setPosts] = useState([1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]);
+    const [posts, setPosts] = useState([]);
+    // const [serverURL, setServerURL] = useState("");
 
-	// insert code here to get post info from the db to render the post
-	// Info Including:
-	// - Post Title
-	// - Post Content
-	// - Post Reacts
-	// - Post Comment Count
-	// const fetchPosts = async () => {
-	// 	try {
+    // insert code here to get post info from the db to render the post
+    // Info Including:
+    // - Post Title
+    // - Post Content
+    // - Post Reacts
+    // - Post Comment Count
 
-	// 		const { data } = await axios.get('');
-	// 		console.log("DATA:",data);
-	// 		setPosts(data);
-	// 	} catch (err) {
-	// 		throw new Error(err.message);
-	// 	}
-	// }
+    // useEffect(() => {
+    // 	console.log("firing useEffect1")
+    //     setServerURL(storeServerURL);
+    // }, []);
 
-	// useEffect(()=> {
-	// 	fetchPosts();
-	// },[])
 
-	// <ForumPost title=(post.title)/>
-	// {JSON.stringify(post)}
+    // Hardcoded URL for now until we can get the env detection working!!!
+    useEffect(() => {
+        console.log("entering useEffect");
+        const fetchPosts = async () => {
+            try {
+                console.log("Entering Try Block...");
+                let myURL = "https://read-herring.herokuapp.com";
+                const { data } = await axios.get(`${myURL}/forums`);
+                data.map((d, idx) => {
+                    d.key = idx;
+                });
+                console.log("DATA:", data);
+                setPosts(data);
+            } catch (err) {
+                throw new Error(err.message);
+            }
+        };
+        fetchPosts();
+    }, []);
 
-// Each forum post will be wrapped in a link that will take you to the posts page
-// Here you will be able to add to react and comment on the post
-	return (
-		<div className="post-wrapper">
-			{
-				posts.map((p)=>(
-					
-				<Link to="/forums"><ForumPost/></Link>
-
-				))
-			}
-		</div>
-	);
+    // Each forum post will be wrapped in a link that will take you to the posts page
+    // Here you will be able to add to react and comment on the post
+    return (
+        <div className="post-wrapper">
+            {posts.map((p) => (
+                <Link
+                    to={{
+                        pathname: "/forums/" + p.key,
+                    }}
+                >
+                    <ForumPost
+                        key={p.key}
+                        title={p.title}
+                        username={p.username}
+                        first_message={p.first_message}
+                    />
+                </Link>
+            ))}
+        </div>
+    );
 };
