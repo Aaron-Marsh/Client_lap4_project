e1015e3852856f491ee809edf3db366f3706a81e
alicekres <alice.kreslins@gmail.com>
2022-07-17 11:09:50 +0100

    add PageNotFound folder and route to app.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,3 +3,4 @@ export { ForumsPage } from './ForumsPage';
 export { HomePage } from './HomePage';
 export { ProfilePage } from './ProfilePage';
 export { SearchPage } from './SearchPage';
+export { PageNotFound } from './PageNotFound';
