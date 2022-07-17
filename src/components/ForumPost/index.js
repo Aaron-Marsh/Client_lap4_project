7ad10762ccff874554075589aba30c5209d01532
Chris Pucknell <chris.pucknell@gmail.comgit config --global -lgit config --global user.email chris.pucknell@gmail.com>
2022-07-17 21:59:21 +0100

    update ForumPost component

--- src/components/ForumPost/index.js
+++ src/components/ForumPost/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './style.css';
 
-export const ForumPost = () => {
+export const ForumPost = ({title,username,first_message}) => {
 	return (
 		<div className="container post">
-			<h2>Forum thread</h2>
-			<p>Now this is a story all about how my life got flipped turned upside down and I'd like to take a minute, just sit right there, I'll tell you how I became the prince of a town called Bel-Air</p>
+			<h3>{title}</h3>
+			<h4>Author: {username}</h4>
+			<p>{first_message}</p>
 		</div>
 	);
 };
