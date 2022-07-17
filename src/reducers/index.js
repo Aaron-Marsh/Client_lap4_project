05df297ff0a3e3f4650b29149bcb7269561880a5
Chris Pucknell <chris.pucknell@gmail.comgit config --global -lgit config --global user.email chris.pucknell@gmail.com>
2022-07-17 22:03:33 +0100

    add serverReducer ref to reducers/index.js

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from "redux";
 
 import loggedInReducer from "./loggedInReducer";
 import footerReducer from "./footerReducer";
+import serverReducer from "./serverReducer"
 
 const allReducers = combineReducers({
   loggedIn: loggedInReducer,
   footerClose: footerReducer,
+  serverReducer: serverReducer,
 });
 export default allReducers;
