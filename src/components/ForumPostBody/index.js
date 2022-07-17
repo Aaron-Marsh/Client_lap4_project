f56246a80a31995f7e8a797ae2895f1edd995b04
Chris Pucknell <chris.pucknell@gmail.comgit config --global -lgit config --global user.email chris.pucknell@gmail.com>
2022-07-17 21:59:53 +0100

    update ForumPostBody component

--- src/components/ForumPostBody/index.js
+++ src/components/ForumPostBody/index.js
@@ -1,10 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
 
 export const ForumPostBody = () => {
-	return (
-		<>
-			<h2>Forum threads</h2>
-			<p>Here are loads of interesting threads</p>
-		</>
-	);
+    console.log("HELLO FROM ForumPostBody");
+
+    let { postId } = useParams();
+    const [post, setPost] = useState("");
+
+    const loadData = async () => {
+        try {
+            console.log("Entering Try Block in PostPage");
+            console.log("postId: ", postId);
+            let myURL = "https://read-herring.herokuapp.com";
+            const { data } = await axios.get(`${myURL}/forums`);
+            // let postData = await data.filter((p) => data.indexOf(p) === postId);
+            // data.length > 0
+            //     ? setPost(await data[postId])
+            //     : setPost({ title: "No Post Found" });
+			setPost(await data[postId])
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
+    useEffect(() => {
+        console.log("hello from inside useEffect");
+        loadData();
+        return () => {};
+    }, []);
+
+    return (
+        <div className="container">
+            <h3>{post.title}</h3>
+            <h4>Author: {post.username}</h4>
+            <p>{post.first_message}</p>
+        </div>
+    );
 };
