6ad58aac1eed409e5e2970e9aa4249691319f8fa
Chris Pucknell <chris.pucknell@gmail.comgit config --global -lgit config --global user.email chris.pucknell@gmail.com>
2022-07-17 22:01:58 +0100

    add export PostPage to pages/index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 export { BooksPage } from './BooksPage';
 export { ForumsPage } from './ForumsPage';
+export { PostPage } from './PostPage';
 export { HomePage } from './HomePage';
 export { ProfilePage } from './ProfilePage';
 export { SearchPage } from './SearchPage';
