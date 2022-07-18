19a767a0536dd27ebe2635c38cd5eaf847628a9e
Chris Pucknell <chris.pucknell@gmail.comgit config --global -lgit config --global user.email chris.pucknell@gmail.com>
2022-07-18 14:10:10 +0100

    remove unused state from PostHeader comp

--- src/pages/PostPage/PostHeader/index.js
+++ src/pages/PostPage/PostHeader/index.js
@@ -1,10 +1,7 @@
 import React from "react";
 
-
 export const PostHeader = ({title,username,first_message}) => {
     console.log("HELLO FROM PostHeader");
-
-    const [comment,setComment] = useState("")
 
     return (
         <div className="container">
