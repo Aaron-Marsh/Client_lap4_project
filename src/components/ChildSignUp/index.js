b33daf21a5cb7815e1087f09906d9ef310a109fd
Izgardon <williamsessions23@gmail.com>
2022-07-18 16:42:22 +0100

    login register routes now work

--- src/components/ChildSignUp/index.js
+++ src/components/ChildSignUp/index.js
@@ -1,7 +1,9 @@
 import React, { useState } from "react";
-
 import Modal from "react-bootstrap/Modal";
-import Form from "react-bootstrap/Form";
+import axios from "axios";
+import { useDispatch } from "react-redux";
+
+import { login, setUser } from "../../actions";
 
 export const ChildSignUpModal = (props) => {
   const [signup, setSignup] = useState(null);
@@ -11,7 +13,55 @@ export const ChildSignUpModal = (props) => {
   const [confirmPassword, setConfirmPassword] = useState("");
   const [error, setError] = useState("");
 
-  const onSignUp = (e) => {};
+  const dispatch = useDispatch();
+
+  const onSignUp = async (e) => {
+    if (email === "" || password === "") {
+      setError("Missing username or password!");
+    } else if (password != confirmPassword) {
+      setError("Passwords do not match!");
+    } else {
+      try {
+        let registerDetails = {
+          username,
+          email,
+          password,
+        };
+        let options = {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        };
+
+        const { data } = await axios.post(
+          "http://127.0.0.1:8000/users/register/",
+          JSON.stringify(registerDetails),
+          options
+        );
+
+        if (data.error) {
+          setError(data.error);
+        } else {
+          props.onHide();
+          dispatch(login());
+
+          dispatch(setUser(registerDetails.userName));
+        }
+
+        /* loginError.textContent = "Incorrect email or password"; */
+      } catch (err) {
+        if (!err.response) {
+          setError("No server response!");
+        } else if (err.response.status === 401) {
+          setError(
+            "Unauthorized! Create an account or check your email and password!"
+          );
+        } else {
+          setError("Login failed!");
+        }
+      }
+    }
+  };
 
   const onUsernameChange = (e) => {
     setUsername(e.target.value);
@@ -70,6 +120,7 @@ export const ChildSignUpModal = (props) => {
             placeholder="Confirm password"
             onChange={onConfirmPasswordChange}
           />
+          <div className="login-error">{error}</div>
         </form>
       </Modal.Body>
       <Modal.Footer>
