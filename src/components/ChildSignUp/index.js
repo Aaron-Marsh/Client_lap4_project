0dd0361f8d82839ffec54fd57db944c8b38a689d
Izgardon <williamsessions23@gmail.com>
2022-07-18 18:17:09 +0100

    fixed commenting

--- src/components/ChildSignUp/index.js
+++ src/components/ChildSignUp/index.js
@@ -1,145 +1,142 @@
-import React, { useState } from 'react';
-import Modal from 'react-bootstrap/Modal';
-import axios from 'axios';
-import { useDispatch } from 'react-redux';
+import React, { useState } from "react";
+import Modal from "react-bootstrap/Modal";
+import axios from "axios";
+import { useDispatch } from "react-redux";
 
-import { login, setUser } from '../../actions';
+import { login, setUser } from "../../actions";
 
 export const ChildSignUpModal = (props) => {
-    const [signup, setSignup] = useState(null);
-    const [username, setUsername] = useState('');
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [confirmPassword, setConfirmPassword] = useState('');
-    const [error, setError] = useState('');
+  const [signup, setSignup] = useState(null);
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [error, setError] = useState("");
 
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-    const onSignUp = async (e) => {
-        if (email === '' || password === '') {
-            setError('Missing username or password!');
-        } else if (password != confirmPassword) {
-            setError('Passwords do not match!');
+  const onSignUp = async (e) => {
+    if (email === "" || password === "") {
+      setError("Missing username or password!");
+    } else if (password != confirmPassword) {
+      setError("Passwords do not match!");
+    } else {
+      try {
+        let registerDetails = {
+          username,
+          email,
+          password,
+        };
+        let options = {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        };
+
+        const { data } = await axios.post(
+          "http://127.0.0.1:8000/users/register/",
+          JSON.stringify(registerDetails),
+          options
+        );
+
+        if (data.error) {
+          setError(data.error);
         } else {
-            try {
-                let registerDetails = {
-                    username,
-                    email,
-                    password,
-                };
-                let options = {
-                    headers: {
-                        'Content-Type': 'application/json',
-                    },
-                };
+          props.onHide();
+          dispatch(login());
 
-                const { data } = await axios.post(
-                    'http://127.0.0.1:8000/users/register/',
-                    JSON.stringify(registerDetails),
-                    options
-                );
-
-                if (data.error) {
-                    setError(data.error);
-                } else {
-                    props.onHide();
-                    dispatch(login());
-
-                    dispatch(setUser(registerDetails.userName));
-                }
-
-                /* loginError.textContent = "Incorrect email or password"; */
-            } catch (err) {
-                if (!err.response) {
-                    setError('No server response!');
-                } else if (err.response.status === 401) {
-                    setError(
-                        'Unauthorized! Create an account or check your email and password!'
-                    );
-                } else {
-                    setError('Login failed!');
-                }
-            }
+          dispatch(setUser(registerDetails.username));
         }
-    };
 
-    const onUsernameChange = (e) => {
-        setUsername(e.target.value);
-    };
+        /* loginError.textContent = "Incorrect email or password"; */
+      } catch (err) {
+        if (!err.response) {
+          setError("No server response!");
+        } else if (err.response.status === 401) {
+          setError(
+            "Unauthorized! Create an account or check your email and password!"
+          );
+        } else {
+          setError("Login failed!");
+        }
+      }
+    }
+  };
 
-    const onEmailChange = (e) => {
-        setEmail(e.target.value);
-    };
+  const onUsernameChange = (e) => {
+    setUsername(e.target.value);
+  };
 
-    const onPasswordChange = (e) => {
-        setPassword(e.target.value);
-    };
+  const onEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
 
-    const onConfirmPasswordChange = (e) => {
-        setConfirmPassword(e.target.value);
-    };
+  const onPasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
 
-    return (
-        <>
-            <Modal.Header className="align-items-center">
-                <Modal.Title
-                    id="contained-modal-title-vcenter"
-                    className="ms-auto"
-                >
-                    Sign Up
-                </Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-                <form className="register">
-                    <label htmlFor="register-username"></label>
-                    <input
-                        type="text"
-                        id="register-username"
-                        required
-                        placeholder="Username"
-                        onChange={onUsernameChange}
-                        aria-label="username"
-                    />
-                    <label htmlFor="register-email"></label>
-                    <input
-                        type="text"
-                        id="register-email"
-                        required
-                        placeholder="Email"
-                        onChange={onEmailChange}
-                        aria-label="email"
-                    />
-                    <label htmlFor="register-password"></label>
-                    <input
-                        type="password"
-                        id="register-password"
-                        required
-                        placeholder="Password"
-                        onChange={onPasswordChange}
-                        aria-label="password"
-                    />
-                    <label htmlFor="confirm-register-password"></label>
-                    <input
-                        type="password"
-                        id="confirm-register-password"
-                        required
-                        placeholder="Confirm password"
-                        onChange={onConfirmPasswordChange}
-                        aria-label="confirm-password"
-                    />
-                    <div className="login-error">{error}</div>
-                </form>
-            </Modal.Body>
-            <Modal.Footer>
-                <button onClick={onSignUp}>Sign up</button>
-                <button
-                    aria-label="toggle-to-log-in"
-                    id="toggle"
-                    onClick={() => props.setShowSignUp(false)}
-                >
-                    Already have an account? Sign in
-                </button>
-            </Modal.Footer>
-        </>
-    );
+  const onConfirmPasswordChange = (e) => {
+    setConfirmPassword(e.target.value);
+  };
+
+  return (
+    <>
+      <Modal.Header className="align-items-center">
+        <Modal.Title id="contained-modal-title-vcenter" className="ms-auto">
+          Sign Up
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <form className="register">
+          <label htmlFor="register-username"></label>
+          <input
+            type="text"
+            id="register-username"
+            required
+            placeholder="Username"
+            onChange={onUsernameChange}
+            aria-label="username"
+          />
+          <label htmlFor="register-email"></label>
+          <input
+            type="text"
+            id="register-email"
+            required
+            placeholder="Email"
+            onChange={onEmailChange}
+            aria-label="email"
+          />
+          <label htmlFor="register-password"></label>
+          <input
+            type="password"
+            id="register-password"
+            required
+            placeholder="Password"
+            onChange={onPasswordChange}
+            aria-label="password"
+          />
+          <label htmlFor="confirm-register-password"></label>
+          <input
+            type="password"
+            id="confirm-register-password"
+            required
+            placeholder="Confirm password"
+            onChange={onConfirmPasswordChange}
+            aria-label="confirm-password"
+          />
+          <div className="login-error">{error}</div>
+        </form>
+      </Modal.Body>
+      <Modal.Footer>
+        <button onClick={onSignUp}>Sign up</button>
+        <button
+          aria-label="toggle-to-log-in"
+          id="toggle"
+          onClick={() => props.setShowSignUp(false)}
+        >
+          Already have an account? Sign in
+        </button>
+      </Modal.Footer>
+    </>
+  );
 };
--- src/pages/PostPage/index.js
+++ src/pages/PostPage/index.js
@@ -1,70 +1,67 @@
 import React, { useState, useEffect } from "react";
-import {useSelector} from 'react-redux'
+import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
-import { LoginFooter } from '../../components/index'
-import { PostHeader } from './PostHeader'
-import { PostComments } from './PostComments'
-import { NewCommentForm } from './NewCommentForm'
+import { LoginFooter } from "../../components/index";
+import { PostHeader } from "./PostHeader";
+import { PostComments } from "./PostComments";
+import { NewCommentForm } from "./NewCommentForm";
 import axios from "axios";
-import './style.css'
+import "./style.css";
 
 export const PostPage = () => {
-    const username = useSelector((state) => state.user.user);
-    const loggedIn = useSelector((state) => state.loggedIn);
-    const [postMessages,setPostMessages] = useState([])
-    const [post, setPost] = useState("");
-    let { postId } = useParams();
+  const username = useSelector((state) => state.user.user);
+  const loggedIn = useSelector((state) => state.loggedIn);
+  const [postMessages, setPostMessages] = useState([]);
+  const [post, setPost] = useState("");
+  let { postId } = useParams();
 
-    const loadData = async () => {
-        try {
-            let myURL = "https://read-herring.herokuapp.com";
-            const { data } = await axios.get(`${myURL}/forums/${postId}`);
-            // const pertinentPost = await data.filter((p)=>{
-            //     return p.id === postId
-            // })
-            setPost(await data)
-        } catch (err) {
-            console.log(err);
-        }
-    };
-    // get post data by id in route
-    useEffect(() => {
-        loadData();
-        return () => {};
-    }, []);
+  const loadData = async () => {
+    try {
+      let myURL = "https://read-herring.herokuapp.com";
+      const { data } = await axios.get(`${myURL}/forums/${postId}`);
+      // const pertinentPost = await data.filter((p)=>{
+      //     return p.id === postId
+      // })
+      setPost(await data);
+    } catch (err) {}
+  };
+  // get post data by id in route
+  useEffect(() => {
+    loadData();
+    return () => {};
+  }, []);
 
-    useEffect(()=>{
-        if (post) {
-            setPostMessages(post.messages)
-            post.messages && post.messages.map((m)=>{
-            })
-        }
-        return () => {};
-    },[post])
+  useEffect(() => {
+    if (post) {
+      setPostMessages(post.messages);
+      post.messages && post.messages.map((m) => {});
+    }
+    return () => {};
+  }, [post]);
 
-    return (
-        <>
-            <div className="container">
-                <h2>PostPage</h2>
-                <pre>Post ID: {postId}</pre>
-                <PostHeader 
-                    title={post.title} 
-                    post_username={post.username}
-                    first_message={post.first_message}
-                />
-                <NewCommentForm 
-                    postId={postId}
-                    onComment={loadData}
-                    username={username}
-                    loggedIn={loggedIn}
-                />
-                {postMessages
-                ? <PostComments 
-                    postMessages={postMessages} 
-                    test="test"/> 
-                : <p>no comments yet</p>}
-            </div>
-            <LoginFooter />
-        </>
-    );
+  return (
+    <>
+      <div className="container">
+        <h2>PostPage</h2>
+        <pre>Post ID: {postId}</pre>
+        <PostHeader
+          title={post.title}
+          post_username={post.username}
+          first_message={post.first_message}
+        />
+        <NewCommentForm
+          postId={postId}
+          onComment={loadData}
+          username={username}
+          loggedIn={loggedIn}
+        />
+        {postMessages ? (
+          <PostComments postMessages={postMessages} test="test" />
+        ) : (
+          <p>no comments yet</p>
+        )}
+      </div>
+      <LoginFooter />
+    </>
+  );
 };
