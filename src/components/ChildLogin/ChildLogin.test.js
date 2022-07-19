8a47ce9f7d711afe58f747fcb5f14d0df47ddbb3
alicekres <alice.kreslins@gmail.com>
2022-07-19 14:30:09 +0100

    trying to test api points in user login and register routes

--- src/components/ChildLogin/ChildLogin.test.js
+++ src/components/ChildLogin/ChildLogin.test.js
@@ -4,12 +4,13 @@ import { Provider } from 'react-redux';
 import React, { useSelector } from 'react';
 import store from '../../store';
 
-import { rest } from 'msw';
+import { rest, setupWorker } from 'msw';
 import { setupServer } from 'msw/node';
 
 import '@testing-library/jest-dom';
 
 import { ChildLoginModal } from './index';
+import { Navbar } from '../Navbar';
 
 import Fetch from './fetch';
 
@@ -26,7 +27,6 @@ describe('ChildLogin', () => {
         fireEvent.change(emailInput, { target: { value: 'Fred@test.com' } });
         expect(emailInput.value).toBe('Fred@test.com');
     });
-
     test('setPassword value is updated', () => {
         const { getByLabelText } = render(
             <Provider store={store}>
@@ -39,7 +39,6 @@ describe('ChildLogin', () => {
         fireEvent.change(passwordInput, { target: { value: 'beautiful' } });
         expect(passwordInput.value).toBe('beautiful');
     });
-
     test('create account button exists', () => {
         render(
             <Provider store={store}>
@@ -52,7 +51,6 @@ describe('ChildLogin', () => {
             screen.getByRole('button', { name: 'toggle-to-sign-up' })
         ).not.toBeDisabled();
     });
-
     test('toggle button exists for switching to signup modal', () => {
         const { getByRole } = render(
             <Provider store={store}>
@@ -67,6 +65,59 @@ describe('ChildLogin', () => {
         fireEvent.click(toggleButton);
         expect(toggleButton).toBeInTheDocument();
     });
+});
+
+describe('ChildLogin following msw example', () => {
+    // const worker = setupWorker(rest.post(''));
+    const server = setupServer(
+        rest.post('/users/login', (req, res, ctx) => {
+            return res(ctx.json({ token: 'mocked_user_token' }));
+        })
+    );
+
+    beforeAll(() => server.listen());
+    afterEach(() => server.resetHandlers());
+    afterAll(() => server.close());
+
+    test('allows the user to log in', async () => {
+        render(
+            <Provider store={store}>
+                <BrowserRouter>
+                    <ChildLoginModal />
+                </BrowserRouter>
+            </Provider>
+        );
+        userEvent.type(screen.getByLabelText('login-email'), 'bunny');
+
+        userEvent.type(screen.getByLabelText('password'), 'secret');
+
+        fireEvent.click(
+            screen.getByRole('button', {
+                name: 'Sign in',
+            })
+        );
+        const button = await screen.findByRole('button', {
+            name: 'Sign in',
+        });
+
+        // Assert successful login state
+        expect(button).toHaveTextContent(/Sign in/i);
+        window.sessionStorage.setItem('token', 'mocked_user_token');
+        expect(window.sessionStorage.getItem('token')).toEqual(
+            'mocked_user_token'
+        );
+    });
+});
+
+test('handles login exception', () => {
+    server.use(
+        rest.post('/users/login', (req, res, ctx) => {
+            return res(
+                ctx.status(500),
+                ctx.json({ message: 'Internal Server Error' })
+            );
+        })
+    );
 });
 
 const server = setupServer(
