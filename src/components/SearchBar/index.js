8f98bbc4a793e771ad7b0d89097713ab47f6f042
Rebecca Blixt <rebecca.blixt@hotmail.com>
2022-07-19 20:18:58 +0200

    add styling booksresult

--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -1,28 +1,35 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 // import axios from 'axios';
 // import { useSelector, useDispatch } from 'react-redux';
 
 export const SearchBar = ({ getResults }) => {
-  // checks if user is logged in for search results
-  // const isLoggedIn = useSelector((state) => state.isLoggedIn);
-  const [search, setSearch] = useState("");
+	// checks if user is logged in for search results
+	// const isLoggedIn = useSelector((state) => state.isLoggedIn);
+	const [search, setSearch] = useState('');
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    getResults(search);
-    console.log(search);
-  };
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		getResults(search);
+		console.log(search);
+	};
 
-  const updateInput = (e) => {
-    const input = e.target.value;
-    setSearch(input);
-  };
+	const updateInput = (e) => {
+		const input = e.target.value;
+		setSearch(input);
+	};
 
-  return (
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="search">Search</label>
-      <input id="search" type="text" onChange={updateInput} value={search} />
-      <input type="submit" value="Search" />
-    </form>
-  );
+	return (
+		<form onSubmit={handleSubmit}>
+			<label htmlFor='search'>Search</label>
+			<input
+				id='search'
+				type='text'
+				placeholder='Search for books here...'
+				className='orange-input'
+				onChange={updateInput}
+				value={search}
+			/>
+			<input type='submit' className='orange-button' value='Search' />
+		</form>
+	);
 };
