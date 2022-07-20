8d89749426759f5cab96e030e28deb9739cd58e3
Rebecca Blixt <rebecca.blixt@hotmail.com>
2022-07-20 11:54:19 +0200

    change import of Books

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,7 @@
 export { BackButton } from './BackButton';
 export { BookModal } from './BookModal';
 export { BooksResult } from './BooksResult';
+export { Books } from './Books';
 export { HomeIntro } from './HomeIntro';
 export { HomeRandom } from './HomeRandom';
 export { LoginModal } from './LoginModal';
