ceb46f4035493c3eb980fd14eaf46cd4055ce347
Chris Pucknell <chris.pucknell@gmail.comgit config --global -lgit config --global user.email chris.pucknell@gmail.com>
2022-07-20 12:04:00 +0100

    add create post button to reveal new thread form

--- src/pages/ForumsPage/NewPostForm/index.js
+++ src/pages/ForumsPage/NewPostForm/index.js
@@ -1,9 +1,28 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import axios from "axios";
 
-export const NewPostForm = ({ username, loggedIn }) => {
+export const NewPostForm = ({ username, loggedIn,onCreate }) => {
   const [title, setTitle] = useState("");
   const [message, setMessage] = useState("");
+  const [isWidth, setIsWidth] = useState(false);
+  const [isShown, setIsShown] = useState(false);
+  const [isNewPostButton, setIsNewPostButton] = useState(true);
+
+  const newPostRef = useRef(null);
+
+    const handleShown = () => {
+    setIsShown((current) => !current);
+    console.log("isShown",isShown)
+  };
+  
+    const handleWidth = () => {
+    setIsWidth((current) => !current);
+    console.log("isWidth",isWidth)
+  };
+
+  const handleNewPostButton = () => {
+    setIsNewPostButton((current) => !current);
+  }
 
   const handleTitleInput = (e) => setTitle(e.target.value);
   const handleMessageInput = (e) => setMessage(e.target.value);
@@ -11,6 +30,9 @@ export const NewPostForm = ({ username, loggedIn }) => {
   const handleFormEvent = async (e) => {
     e.preventDefault();
     e.stopPropagation();
+    handleNewPostButton();
+    handleWidth();
+    handleShown();
     const serverUrl = "https://read-herring.herokuapp.com";
     try {
       const res = await axios({
@@ -23,12 +45,39 @@ export const NewPostForm = ({ username, loggedIn }) => {
         },
       });
     } catch (err) {}
+    // refresh posts
+    onCreate();
   };
+
   return (
-    <>
-      <form onSubmit={handleFormEvent}>
-        <label htmlFor="Title">Post Title</label>
+    <div className="new-post-form-container-outer">
+      {isNewPostButton ? 
+      <button
+      className="orange-button-wide"
+      onClick={() => {
+        handleShown();
+        handleWidth();
+        handleNewPostButton();
+        setTimeout(() => {
+          newPostRef.current.focus();
+        }, 100);
+      }}
+    >
+      Create New Post
+    </button>
+    :   
+    (
+      <div className="new-post-form-container-inner">
+      <form 
+      onSubmit={handleFormEvent}
+      className={
+        isShown && isWidth ? "new-post-input width"
+            : "new-post-input "
+      }
+      >
+        <label htmlFor="Title">Title</label>
         <input
+          ref={newPostRef}
           type="text"
           id="title"
           name="title"
@@ -36,17 +85,31 @@ export const NewPostForm = ({ username, loggedIn }) => {
           value={title}
           onChange={handleTitleInput}
         />
-        <label htmlFor="message">Post message</label>
-        <input
-          type="text"
+        <label htmlFor="message">Message</label>
+        <textarea
           id="message"
           name="message"
           placeholder="Type here..."
           value={message}
           onChange={handleMessageInput}
         />
-        <input type="submit" disabled={!loggedIn} />
+        <input 
+        type="submit"
+        className="orange-button" 
+        disabled={!loggedIn} 
+        value="Post"/>
       </form>
-    </>
+      <div
+        className="close-new-post-field"
+        onClick={() => {
+          handleShown();
+          handleWidth();
+          handleNewPostButton();
+        }}
+    ></div>
+      </div>
+    )
+      }
+    </div>
   );
 };
