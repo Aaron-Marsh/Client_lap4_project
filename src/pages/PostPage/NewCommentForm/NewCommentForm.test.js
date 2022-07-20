b8cb572692d82cdf15d3573bff6f80a0aa149b7f
alicekres <alice.kreslins@gmail.com>
2022-07-20 16:14:01 +0100

    add 3 tests to NewCommentFom

---
+++ src/pages/PostPage/NewCommentForm/NewCommentForm.test.js
@@ -0,0 +1,54 @@
+import { screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from '../../../store';
+
+import { NewCommentForm } from '.';
+
+describe('NewCommentForm', () => {
+    beforeEach(() => {
+        render(
+            <Provider store={store}>
+                <BrowserRouter>
+                    <NewCommentForm
+                        postId={null}
+                        onComment={''}
+                        username={''}
+                        loggedIn={'true'}
+                        serverURL={''}
+                    />
+                </BrowserRouter>
+            </Provider>
+        );
+    });
+
+    test('it should render textarea element', () => {
+        const textarea = screen.getByPlaceholderText(/Add a comment here.../i);
+        expect(textarea).toBeInTheDocument();
+    });
+
+    test('should be able to type into textarea', () => {
+        const textarea = screen.getByPlaceholderText(/Add a comment here.../i);
+
+        fireEvent.change(textarea, {
+            target: { value: 'I like how practical this book is! Love it!' },
+        });
+        expect(textarea.value).toBe(
+            'I like how practical this book is! Love it!'
+        );
+    });
+
+    test('should have emtpy textarea when ', () => {
+        const textarea = screen.getByPlaceholderText(/Add a comment here.../i);
+        const button = screen.getByRole('button');
+
+        fireEvent.change(textarea, {
+            target: { value: 'I like how practical this book is! Love it!' },
+        });
+
+        fireEvent.click(button);
+        expect(textarea.value).toBe(
+            'I like how practical this book is! Love it!'
+        );
+    });
+});
--- src/pages/PostPage/PostComment/index.js
+++ src/pages/PostPage/PostComment/index.js
@@ -1,203 +1,209 @@
-import React, { useState, useEffect, useRef } from "react";
-import axios from "axios";
-import { Link } from "react-router-dom";
+import React, { useState, useEffect, useRef } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 export const PostComment = ({
-  message_username,
-  message,
-  replies,
-  loggedIn,
-  postId,
-  serverURL,
-  messageId,
-  username,
+    message_username,
+    message,
+    replies,
+    loggedIn,
+    postId,
+    serverURL,
+    messageId,
+    username,
 }) => {
-  const [reply, setReply] = useState("");
-  const [isShown, setIsShown] = useState(false);
-  const [isReplyButton, setIsReplyButton] = useState(true);
-  const [isWidth, setIsWidth] = useState(false);
-  const [repliesArray, setRepliesArray] = useState([]);
-  const [deleted, setDeleted] = useState(false);
+    const [reply, setReply] = useState('');
+    const [isShown, setIsShown] = useState(false);
+    const [isReplyButton, setIsReplyButton] = useState(true);
+    const [isWidth, setIsWidth] = useState(false);
+    const [repliesArray, setRepliesArray] = useState([]);
+    const [deleted, setDeleted] = useState(false);
 
-  const replyRef = useRef(null);
+    const replyRef = useRef(null);
 
-  let clicked = false;
-  let width = false;
+    let clicked = false;
+    let width = false;
 
-  const handleReplyInput = (e) => setReply(e.target.value);
-  const handleWidth = () => {
-    setIsWidth((current) => !current);
-  };
-  const handleShown = () => {
-    setIsShown((current) => !current);
-  };
-  const handleReplyButton = () => {
-    setIsReplyButton((current) => !current);
-  };
+    const handleReplyInput = (e) => setReply(e.target.value);
+    const handleWidth = () => {
+        setIsWidth((current) => !current);
+    };
+    const handleShown = () => {
+        setIsShown((current) => !current);
+    };
+    const handleReplyButton = () => {
+        setIsReplyButton((current) => !current);
+    };
 
-  useEffect(() => {
-    setRepliesArray(replies);
-  }, []);
+    useEffect(() => {
+        setRepliesArray(replies);
+    }, []);
 
-  const handleReplyEvent = async (e) => {
-    e.preventDefault();
-    e.stopPropagation();
-    try {
-      const { data } = await axios({
-        method: "PATCH",
-        url: `https://read-herring.herokuapp.com/forums/${postId}`,
-        data: {
-          method: "reply_message",
-          username: username,
-          reply: reply,
-          message_id: messageId,
-          reply_to: "",
-        },
-      });
-      setRepliesArray((current) => [...current, data]);
+    const handleReplyEvent = async (e) => {
+        e.preventDefault();
+        e.stopPropagation();
+        try {
+            const { data } = await axios({
+                method: 'PATCH',
+                url: `https://read-herring.herokuapp.com/forums/${postId}`,
+                data: {
+                    method: 'reply_message',
+                    username: username,
+                    reply: reply,
+                    message_id: messageId,
+                    reply_to: '',
+                },
+            });
+            setRepliesArray((current) => [...current, data]);
 
-      setReply("");
-    } catch (err) {}
-  };
+            setReply('');
+        } catch (err) {}
+    };
 
-  const handleDeleteReplyEvent = async () => {
-    try {
-      const { data } = await axios({
-        method: "PATCH",
-        url: `https://read-herring.herokuapp.com/forums/${postId}`,
-        data: {
-          method: "delete_message",
+    const handleDeleteReplyEvent = async () => {
+        try {
+            const { data } = await axios({
+                method: 'PATCH',
+                url: `https://read-herring.herokuapp.com/forums/${postId}`,
+                data: {
+                    method: 'delete_message',
 
-          message_id: messageId,
-        },
-      });
-      console.log(message_username, username);
-      setDeleted(true);
-    } catch (err) {}
-  };
+                    message_id: messageId,
+                },
+            });
+            console.log(message_username, username);
+            setDeleted(true);
+        } catch (err) {}
+    };
 
-  return (
+    return (
+        <>
+            {' '}
+            {/* ******** Boolean Toggle for Hiding Deleted Comments ******** */}
+            {!deleted && (
+                <div className="message-box">
+                    {message_username == username && loggedIn ? (
+                        <div
+                            className="delete-message"
+                            onClick={() => {
+                                handleDeleteReplyEvent();
+                            }}
+                        ></div>
+                    ) : (
+                        ''
+                    )}
 
-    <>
-      {" "}
-      {/* ******** Boolean Toggle for Hiding Deleted Comments ******** */}
-      {!deleted && (
-        <div className="message-box">
-          {message_username == username && loggedIn ? (
-            <div
-              className="delete-message"
-
-              onClick={() => {
-                handleDeleteReplyEvent();
-              }}
-            ></div>
-          ) : (
-            ""
-          )}
-
-          <div className="message-content">
-            <div
-              className="message-line"
-              onClick={() => {
-                clicked
-                  ? document
-                      .getElementById(messageId)
-                      .classList.remove("hide-reply")
-                  : document
-                      .getElementById(messageId)
-                      .classList.add("hide-reply");
-                clicked = !clicked;
-              }}
-            ></div>
-            <div>
-              <Link
-                className="message-username"
-                to={`/profile/${message_username}`}
-              >
-                {message_username}
-              </Link>
-            </div>
-            <div className="message-message">{message}</div>
-          </div>
-          <div className="reply-container" id={messageId}>
-            {repliesArray.length > 0
-              ? repliesArray.map((reply) => (
-                  <div className="reply-content">
-                    <div>
-                      <Link
-                        className="message-username"
-                        to={`/profile/${reply.username}`}
-                      >
-                        {reply.username}
-                      </Link>
+                    <div className="message-content">
+                        <div
+                            className="message-line"
+                            aria-label="message-line"
+                            onClick={() => {
+                                clicked
+                                    ? document
+                                          .getElementById(messageId)
+                                          .classList.remove('hide-reply')
+                                    : document
+                                          .getElementById(messageId)
+                                          .classList.add('hide-reply');
+                                clicked = !clicked;
+                            }}
+                        ></div>
+                        <div>
+                            <Link
+                                aria-label="link-to-message-poster"
+                                className="message-username"
+                                to={`/profile/${message_username}`}
+                            >
+                                {message_username}
+                            </Link>
+                        </div>
+                        <div className="message-message">{message}</div>
                     </div>
-                    <div className="reply-message" id={reply.id}>
-                      {reply.reply}
+                    <div
+                        className="reply-container"
+                        aria-label="replies"
+                        id={messageId}
+                    >
+                        {repliesArray.length > 0
+                            ? repliesArray.map((reply) => (
+                                  <div className="reply-content">
+                                      <div>
+                                          <Link
+                                              className="message-username"
+                                              to={`/profile/${reply.username}`}
+                                          >
+                                              {reply.username}
+                                          </Link>
+                                      </div>
+                                      <div
+                                          className="reply-message"
+                                          id={reply.id}
+                                      >
+                                          {reply.reply}
+                                      </div>
+                                  </div>
+                              ))
+                            : ''}
+                        <div className="reply-form-container">
+                            {!isReplyButton ? (
+                                ''
+                            ) : (
+                                <button
+                                    className="white-button"
+                                    onClick={() => {
+                                        handleShown();
+                                        handleWidth();
+                                        handleReplyButton();
+                                        setTimeout(() => {
+                                            replyRef.current.focus();
+                                        }, 100);
+                                    }}
+                                >
+                                    Reply
+                                </button>
+                            )}
+                            <div className="message-replies">
+                                <form
+                                    className={
+                                        isShown
+                                            ? isWidth
+                                                ? 'message-reply-input width'
+                                                : 'message-reply-input '
+                                            : 'message-reply-input'
+                                    }
+                                    onSubmit={handleReplyEvent}
+                                >
+                                    <label htmlFor="reply"></label>
+                                    <input
+                                        ref={replyRef}
+                                        type="text"
+                                        id="reply"
+                                        name="reply"
+                                        className="orange-input"
+                                        placeholder="Add your reply..."
+                                        value={reply}
+                                        onChange={handleReplyInput}
+                                    />
+                                    <input
+                                        className="orange-button"
+                                        type="submit"
+                                        value="Send"
+                                        disabled={!loggedIn}
+                                    />
+                                    <div
+                                        className="close-reply-field"
+                                        onClick={() => {
+                                            handleShown();
+                                            handleWidth();
+                                            handleReplyButton();
+                                        }}
+                                    ></div>
+                                </form>
+                            </div>
+                        </div>
                     </div>
-                  </div>
-                ))
-              : ""}
-            <div className="reply-form-container">
-              {!isReplyButton ? (
-                ""
-              ) : (
-                <button
-                  className="white-button"
-                  onClick={() => {
-                    handleShown();
-                    handleWidth();
-                    handleReplyButton();
-                    setTimeout(() => {
-                      replyRef.current.focus();
-                    }, 100);
-                  }}
-                >
-                  Reply
-                </button>
-              )}
-              <div className="message-replies">
-                <form
-                  className={
-                    isShown
-                      ? isWidth
-                        ? "message-reply-input width"
-                        : "message-reply-input "
-                      : "message-reply-input"
-                  }
-                  onSubmit={handleReplyEvent}
-                >
-                  <label htmlFor="reply"></label>
-                  <input
-                    ref={replyRef}
-                    type="text"
-                    id="reply"
-                    name="reply"
-                    className="orange-input"
-                    placeholder="Add your reply..."
-                    value={reply}
-                    onChange={handleReplyInput}
-                  />
-                  <input
-                    className="orange-button"
-                    type="submit"
-                    value="Send"
-                    disabled={!loggedIn}
-                  />
-                  <div
-                    className="close-reply-field"
-                    onClick={() => {
-                      handleShown();
-                      handleWidth();
-                      handleReplyButton();
-                    }}
-                  ></div>
-                </form>
-              </div>
-            </div>
-          </div>
-        </div>
-      )}
-    </>
-
-  );
+                </div>
+            )}
+        </>
+    );
 };
