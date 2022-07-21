1067aa558684b6882c4efca41ed03bcd075c3bb0
Chris Pucknell <chris.pucknell@gmail.comgit config --global -lgit config --global user.email chris.pucknell@gmail.com>
2022-07-21 03:25:51 +0100

    add useState to FourmPosts to update posts on screen WIP

--- src/pages/ForumsPage/ForumPosts/index.js
+++ src/pages/ForumsPage/ForumPosts/index.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { ForumPost } from "../ForumPost";
 import { Link } from "react-router-dom";
 
 export const ForumPosts = ({ posts }) => {
+
+  const [currentPosts,setCurrentPosts] = useState([])
+
+  useEffect(()=> {
+    setCurrentPosts(posts)
+  },[posts])
+
   return (
     <div className="posts-container">
-      {posts.map((p) => (
+      {currentPosts.map((p) => (
         <Link
           className="link"
           to={{
